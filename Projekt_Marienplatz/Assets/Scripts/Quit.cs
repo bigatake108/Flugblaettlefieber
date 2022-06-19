a6d063502848612c830c98ae64899b1d8ecfba0f
bigatake108 <marco.madeja@web.de>
2022-06-19 23:07:27 +0200

    "Verlassen" Buttons

--- Projekt_Marienplatz/Assets/Scripts/Quit.cs
+++ Projekt_Marienplatz/Assets/Scripts/Quit.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class Quit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Quits the game
+    public void QuitGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Application.Quit();
     }
 }
